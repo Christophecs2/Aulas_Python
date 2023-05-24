366a94322ec7f03f7aa8b1bc623a57ef97fe25b7
Christophe Costa <costachristophe065@gmail.com>
2023-05-24 14:25:54 -0300

    Delete input_exerc.py

--- input_exerc.py
+++
@@ -1,8 +0,0 @@
-entrada = input(' Digite oq vc deseja : entrar ou sair:  ')
-
-if entrada == 'entrar':
-    print('Vc escolheu entrar, ok obg')
-elif entrada == 'sair':
-    print('Vc escolheu sair, ok Obrigado')
-else:
-    print('Vc n escolheu nada validado mds...')
